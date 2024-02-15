27c01d3610e537263005123393e242da13eb2bbc
Solaimanislam <islamsi2001@gmail.com>
2024-02-15 12:06:50 +0600

    added javascript file

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -13,7 +13,24 @@
 
 // }
 
+function continueGame(){
+    // step-1: generate a random alphabet 
+    const alphabet = getARandomAlphabet();
+
+    console.log('Your alphabet:',alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color 
+    setBackgroundColorById(alphabet);
+
+
+}
+
 function play(){
     hideElementById('home-screen');
     addElementById('play-ground');
+    continueGame();
 }
